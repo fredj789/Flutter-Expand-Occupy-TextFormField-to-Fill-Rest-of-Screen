02cfa4829b7c5cd1985b9c781bc5a8162a6f7c96
fredj789 <fredj_jomaa@hotmail.com>
2022-10-13 18:41:45 +0200

    Programming flutter

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -11,18 +11,53 @@ class _MyPageHomeState extends State<MyPageHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-        ),
-        body: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                  //suffix: Text("الاسم"), suffixIcon: Icon(Icons.person)),
-                  labelText: "ادخل الاسم ",
-                  labelStyle: TextStyle(color: Colors.red, fontSize: 20)),
-            )
+      appBar: AppBar(
+          //title: Text(widget.title),
+          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("What is this new classroom?"),
+            SizedBox(
+              height: 8.0,
+            ),
+            Expanded(
+              child: Form(
+                // key: _formKey,
+                child:
+                    ListView(padding: EdgeInsets.all(8.0), children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Classroom Name",
+                          hintText: "What's name of the new classroom?",
+                        ),
+                      )),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Description",
+                        hintText: "Description of the new classroom",
+                      ),
+                      //maxLines: 5,
+                    ),
+                  ),
+                ]),
+              ),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
